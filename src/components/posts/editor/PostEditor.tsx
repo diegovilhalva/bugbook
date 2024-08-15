55e0beb89d1093d7d182991bfda02f8eabf71b3c
Diego Vilhalva <diegorocha450@yahoo.com>
2024-08-15 08:24:25 -0300

    mutations added to the for you page

--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -8,9 +8,12 @@ import UserAvatar from "@/components/UserAvatar"
 import { useSession } from "@/app/(main)/SessionProvider"
 import { Button } from "@/components/ui/button"
 import "./styles.css"
+import { useSubmitPostMutation } from "./mutations"
+import LoadingButton from "@/components/LoadingButton"
 export default function PostEditor() {
-    const {user} = useSession()
+    const { user } = useSession()
 
+    const mutation = useSubmitPostMutation()
 
     const editor = useEditor({
         extensions: [
@@ -28,26 +31,30 @@ export default function PostEditor() {
         blockSeparator: "\n",
     }) || ""
 
-    async function onSubmit() {
-        await submitPost(input)
-        editor?.commands.clearContent()
+    function onSubmit() {
+        mutation.mutate(input, {
+            onSuccess: () => {
+                editor?.commands.clearContent()
+            }
+        })
     }
 
     return (
         <div className="flex flex-col gap-5 rounded-2xl bg-card p-5 shadow-sm">
             <div className="flex gap-5">
-                <UserAvatar avatarUrl={user.avatarUrl } className="hidden sm:inline" />
+                <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" />
                 <EditorContent
-                editor={editor}
-                className="w-full max-h-[20rem] overflow-y-auto bg-background rounded-2xl px-5 py-3" /> 
+                    editor={editor}
+                    className="w-full max-h-[20rem] overflow-y-auto bg-background rounded-2xl px-5 py-3" />
             </div>
             <div className="flex justify-end">
-                <Button
+                <LoadingButton
                     onClick={onSubmit}
+                    loading={mutation.isPending}
                     disabled={!input.trim()}
                     className="min-w-20">
-                        Postar
-                    </Button>
+                    Postar
+                </LoadingButton>
             </div>
         </div>
     )
